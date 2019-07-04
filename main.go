5ac319bb1249152637d062b302124488201bd87f
violetgen <violetgen330@gmail.com>
2019-07-04 06:17:51 +0100

    Integrate redis

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 )
+
+var client *redis.Client
 
 var templates *template.Template
 
 func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379", //redis port
+	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandler).Methods("GET")
@@ -24,8 +30,13 @@ func main() {
 }
 
 func indexHandler(res http.ResponseWriter, req *http.Request) {
+	//get the first ten strings in redis from a string called "comments":
+	comments, err := client.LRange("comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
 	// fmt.Fprint(res, "Hello World!")
-	templates.ExecuteTemplate(res, "index.html", nil)
+	templates.ExecuteTemplate(res, "index.html", comments)
 }
 
 // func goodbyeHandler(res http.ResponseWriter, req *http.Request) {
