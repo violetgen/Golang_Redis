c2cb7f29b533f79d697e719224111ba62803e7ef
violetgen <violetgen330@gmail.com>
2019-07-04 06:02:46 +0100

    Using templates

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var templates *template.Template
+
 func main() {
+	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/hello", helloHandler).Methods("GET")
-	r.HandleFunc("/goodbye", goodbyeHandler).Methods("GET")
+	r.HandleFunc("/", indexHandler).Methods("GET")
+	// r.HandleFunc("/goodbye", goodbyeHandler).Methods("GET")
 
 	http.Handle("/", r)
 	http.ListenAndServe(":7000", nil)
@@ -20,10 +23,11 @@ func main() {
 	// http.ListenAndServe(":7000", nil)
 }
 
-func helloHandler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(res, "Hello World!")
+func indexHandler(res http.ResponseWriter, req *http.Request) {
+	// fmt.Fprint(res, "Hello World!")
+	templates.ExecuteTemplate(res, "index.html", nil)
 }
 
-func goodbyeHandler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(res, "Goodbye World!")
-}
+// func goodbyeHandler(res http.ResponseWriter, req *http.Request) {
+// 	fmt.Fprint(res, "Goodbye World!")
+// }
